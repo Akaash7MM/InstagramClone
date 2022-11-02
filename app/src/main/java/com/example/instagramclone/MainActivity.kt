8f920b1002bba21e5dce20bcbc1803f9f4200150
Shubham Singh <shubham.singh@mutualmobile.com>
2022-11-02 18:12:35 +0530

    Fix - Bottom Nav bar doesn't hide

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -6,13 +6,9 @@ import android.transition.TransitionManager
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.instagramclone.databinding.ActivityMainBinding
-import com.example.instagramclone.fragments.LoginFragment
-import com.example.instagramclone.fragments.SearchScreen
 import com.google.android.material.elevation.SurfaceColors
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,34 +23,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        window.setStatusBarColor(SurfaceColors.SURFACE_5.getColor(this))
+        window.statusBarColor = SurfaceColors.SURFACE_5.getColor(this)
         val navHostFragment = binding.fragmentContainerView.getFragment<NavHostFragment>()
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            TransitionManager.beginDelayedTransition(binding.root, Slide(Gravity.END))
+            when (destination.id) {
+                R.id.mainScreen -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+            }
+        }
+
 //        binding.bottomNavigationView.visibility = View.GONE
 
-        supportFragmentManager.registerFragmentLifecycleCallbacks(
-            object : FragmentManager.FragmentLifecycleCallbacks() {
-                override fun onFragmentCreated(
-                    fm: FragmentManager,
-                    f: Fragment,
-                    savedInstanceState: Bundle?
-                ) {
-                    TransitionManager.beginDelayedTransition(binding.root, Slide(Gravity.END))
-                    when (f) {
-                        is LoginFragment -> {
-                        }
-                        is SearchScreen -> {
-                            binding.bottomNavigationView.visibility = View.GONE
-                        }
-                        else -> {
-                            binding.bottomNavigationView.visibility = View.VISIBLE
-                        }
-                    }
-                }
-            },
-            true
-        )
+//        supportFragmentManager.registerFragmentLifecycleCallbacks(
+//            object : FragmentManager.FragmentLifecycleCallbacks() {
+//                override fun onFragmentCreated(
+//                    fm: FragmentManager,
+//                    f: Fragment,
+//                    savedInstanceState: Bundle?
+//                ) {
+//                    TransitionManager.beginDelayedTransition(binding.root, Slide(Gravity.END))
+//                    when (f) {
+//                        is LoginFragment -> {
+//                        }
+//                        is SearchScreen -> {
+//                            binding.bottomNavigationView.visibility = View.GONE
+//                        }
+//                        else -> {
+//                            binding.bottomNavigationView.visibility = View.VISIBLE
+//                        }
+//                    }
+//                }
+//            },
+//            true
+//        )
     }
 }
