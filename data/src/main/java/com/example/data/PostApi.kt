e7c6e2a44177b2e987799ede4c51654f1044156e
Akaash7MM <akashdeep.singh@mutualmobile.com>
2022-11-10 09:25:20 +0530

    chore - refactoring bottom bar,usecase,drawables

--- data/src/main/java/com/example/data/PostApi.kt
+++ data/src/main/java/com/example/data/PostApi.kt
@@ -1,10 +1,11 @@
 package com.example.data
 
 import com.example.data.remote.dto.PostDTO
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface PostApi {
 
     @GET("/v1/curated")
-    suspend fun getPosts(): PostDTO
+    suspend fun getPosts(): Response<PostDTO>
 }
--- domain/src/main/java/com/example/domain/usecases/GetPostUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/GetPostUseCase.kt
@@ -1,9 +1,14 @@
 package com.example.domain.usecases
 
+import com.example.domain.entities.Post
 import com.example.domain.repository.PostRepository
+import com.example.domain.util.Resource
 
 class GetPostUseCase(
     private val postRepository: PostRepository
 ) {
-    suspend operator fun invoke() = postRepository.getPosts()
+    suspend operator fun invoke(): Resource<List<Post>> {
+        val list = postRepository.getPosts()
+        return Resource.Success(list)
+    }
 }
